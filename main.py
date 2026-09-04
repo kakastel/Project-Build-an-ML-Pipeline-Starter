512bfdff634623b11baf9729b2958b67e485470d
Ken Kastel <kkastel@me.com>
2026-09-04 10:15:13 -0400

    Data_split was finished and added

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ def go(config: DictConfig):
             )
 
         if "data_check" in active_steps:
-            _=mlflow.run(
+            _ =mlflow.run(
                 "src/data_check",
                 "main",
                 env_manager="conda",
@@ -79,10 +79,17 @@ def go(config: DictConfig):
             )
 
         if "data_split" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ =mlflow.run(
+                f"{config['main']['components_repository']}/train_val_test_split",
+                "main",
+                env_manager="conda",
+                parameters={
+                    "input": "clean_sample.csv:latest",
+                    "test_size": config["modeling"]["test_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"],
+                }
+            )
 
         if "train_random_forest" in active_steps:
 
